75afd7796bf5591ea09720bb4b93077b569cc3db
Sathiya N Sundararajan <ausathya@gmail.com>
2018-04-06 17:36:57 -0700

    Project Initial Checking

---
+++ src/main/java/com/sat/textfun/TextfunApplication.java
@@ -0,0 +1,12 @@
+package com.sat.textfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextfunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextfunApplication.class, args);
+	}
+}
